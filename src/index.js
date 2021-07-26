dd9d3c00a241aa7cb387197ee80c75bb45a6ec38
Daniel <danprog91@gmail.com>
2021-07-26 11:15:49 -0300

    Implementado cadastro de conta e validação de CPF

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 const express = require('express');
+const { v4: uuidV4, } = require('uuid');
 
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+  const { name, cpf } = request.body;
+
+  const userAlreadyExists = customers.find((user) => user.cpf === cpf);
+
+  if (userAlreadyExists) {
+    return response.status(404).json({ error: "User already exist." });
+  }
+
+  user = {
+    uuid: uuidV4(),
+    name,
+    cpf,
+    statement: [],
+  }
+
+  customers.push(user);
+
+  return response.status(201).send();
+})
 
 app.listen(3333,
   console.log("🚀 Server started on port 3333!")
